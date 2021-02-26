5d82343f3071c77b1a750d1a5cbe57c6c2a24496
“【ikemoti】” <ikepon413@icloud.com>
2021-02-26 16:01:19 +0900

    Initial Commit

---
+++ iOSArchitecturePattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSArchitecturePattern
+//
+//  Created by 池本壮介 on 2021/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
